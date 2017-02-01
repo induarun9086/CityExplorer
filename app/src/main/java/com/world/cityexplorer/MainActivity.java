54ae087c8994679256ee55a808c699aba67a81c7
Indumathi Duraipandian <indumathi.duraipandian@student.fh-kiel.de>
2017-02-01 22:17:20 +0100

    Initial version

---
+++ app/src/main/java/com/world/cityexplorer/MainActivity.java
@@ -0,0 +1,26 @@
+package com.world.cityexplorer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
+public class MainActivity extends AppCompatActivity  implements OnMapReadyCallback{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
